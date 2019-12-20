7f8f7d3c4bd927352ce4f5a981b0b3d1034dcc64
Davide Fiorino <davidefiorino@MacBook-Pro-di-Davide.local>
2019-12-20 11:55:23 +0100

    fix fair loss

--- src/loss/fair.py
+++ src/loss/fair.py
@@ -5,12 +5,12 @@ class Fair(nn.Module):
 
     def __init__(self):
         super(Fair, self).__init__()
-        self.c = 1
+        self.c = 1.0
 
     def forward(self, X, Y):
         r = torch.add(X, -Y)
-        r_a = torch.abs(r)
+        ra = torch.abs(r)
 
-        loss = self.c * self.c * ( r_a/self.c - torch.log(1 + r_a/self.c ) )
+        loss = (self.c **2) * (ra/self.c - torch.log(1 + ra/self.c))
         
         return loss 
