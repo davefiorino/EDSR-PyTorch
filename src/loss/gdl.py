49c05470b7c0f4e1a2be843cecf33644598d44b9
Davide Fiorino <davidefiorino@MacBook-Pro-di-Davide.local>
2019-12-21 21:18:00 +0100

    f

--- src/loss/gdl.py
+++ src/loss/gdl.py
@@ -12,15 +12,16 @@ class GDL(nn.Module):
         
         print(Y_true.shape)
         print(Y_pred.shape)
-        t1 = torch.pow(torch.abs(Y_true[1:, :, :] - Y_true[:-1, :, :]) -
-                   torch.abs(Y_pred[1:, :, :] - Y_pred[:-1, :, :]), self.alpha)
-
-        t2 = torch.pow(torch.abs(Y_true[:, :-1, :] - Y_true[:, 1:, :]) -
-                   torch.abs(Y_pred[:, :-1, :] - Y_pred[:, 1:, :]), self.alpha)
-
+        t1 = torch.pow(torch.abs(Y_true[:, :, 1:, :] - Y_true[:, :, :-1, :]) -
+                   torch.abs(Y_pred[:, :, 1:, :] - Y_pred[:, :, :-1, :]), alpha)
+        t2 = torch.pow(torch.abs(Y_true[:, :, :, :-1] - Y_true[:, :, :, 1:]) -
+                   torch.abs(Y_pred[:, :, :, :-1] - Y_pred[:, :, :, 1:]), alpha)
+        
         print(t1.shape)
         print(t2.shape)
-        
+
+        out = torch.mean(torch.reshape(t1 + t2, -1), -1)
+
         error = torch.add(t1, t2)
         loss = torch.sum(error)
         return loss 
