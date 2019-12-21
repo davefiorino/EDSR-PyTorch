3595dbd21697862442fc6a7db63dd6827c251864
Davide Fiorino <davidefiorino@MacBook-Pro-di-Davide.local>
2019-12-21 21:49:29 +0100

    f

--- src/loss/gdl.py
+++ src/loss/gdl.py
@@ -17,14 +17,12 @@ class GDL(nn.Module):
         Y_trueR = torch.from_numpy(np.flip(Y_true.detach().cpu().numpy(), 2).copy()).cuda()
         Y_predR = torch.from_numpy(np.flip(Y_pred.detach().cpu().numpy(), 2).copy()).cuda()
 
-        t1 = torch.pow(torch.abs(Y_true - Y_trueR) -
-                   torch.abs(Y_pred - Y_predR), self.alpha)
+        t1 = torch.pow(torch.abs(Y_true - Y_trueR) - torch.abs(Y_pred - Y_predR), self.alpha)
 
         Y_trueR = torch.from_numpy(np.flip(Y_true.detach().cpu().numpy(), 3).copy()).cuda()
         Y_predR = torch.from_numpy(np.flip(Y_pred.detach().cpu().numpy(), 3).copy()).cuda()
 
-        t2 = torch.pow(torch.abs(Y_trueR - Y_true) -
-                   torch.abs(Y_predE - Y_pred), self.alpha)
+        t2 = torch.pow(torch.abs(Y_trueR - Y_true) - torch.abs(Y_predR - Y_pred), self.alpha)
         
         print(t1.shape)
         print(t2.shape)
