00f8612d2ac666cee513d3755adf3415a65469f9
Davide Fiorino <davidefiorino@MacBook-Pro-di-Davide.local>
2019-12-21 23:20:51 +0100

    f

--- src/loss/gdl.py
+++ src/loss/gdl.py
@@ -11,13 +11,13 @@ class GDL(nn.Module):
 
     def forward(self, Y_true, Y_pred):
 
-        Y_trueR = torch.from_numpy(np.flip(Y_true.detach().cpu().numpy(), 2).copy()).cuda()
-        Y_predR = torch.from_numpy(np.flip(Y_pred.detach().cpu().numpy(), 2).copy()).cuda()
+        Y_trueR = torch.from_numpy(np.flip(Y_true.cpu().numpy(), 2).copy()).cuda()
+        Y_predR = torch.from_numpy(np.flip(Y_pred.cpu().numpy(), 2).copy()).cuda()
 
         t1 = torch.pow(torch.abs(Y_true - Y_trueR) - torch.abs(Y_pred - Y_predR), self.alpha)
 
-        Y_trueR = torch.from_numpy(np.flip(Y_true.detach().cpu().numpy(), 3).copy()).cuda()
-        Y_predR = torch.from_numpy(np.flip(Y_pred.detach().cpu().numpy(), 3).copy()).cuda()
+        Y_trueR = torch.from_numpy(np.flip(Y_true.cpu().numpy(), 3).copy()).cuda()
+        Y_predR = torch.from_numpy(np.flip(Y_pred.cpu().numpy(), 3).copy()).cuda()
 
         t2 = torch.pow(torch.abs(Y_trueR - Y_true) - torch.abs(Y_predR - Y_pred), self.alpha)
 
