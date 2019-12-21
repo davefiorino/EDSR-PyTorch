00c33fbb94b46870cb41264a5dfd4b243d41fdab
Davide Fiorino <davidefiorino@MacBook-Pro-di-Davide.local>
2019-12-21 21:14:28 +0100

    fix gdl

--- src/loss/gdl.py
+++ src/loss/gdl.py
@@ -10,12 +10,17 @@ class GDL(nn.Module):
 
     def forward(self, Y_true, Y_pred):
         
-
+        print(Y_true.shape)
+        print(Y_pred.shape)
         t1 = torch.pow(torch.abs(Y_true[1:, :, :] - Y_true[:-1, :, :]) -
                    torch.abs(Y_pred[1:, :, :] - Y_pred[:-1, :, :]), self.alpha)
+
         t2 = torch.pow(torch.abs(Y_true[:, :-1, :] - Y_true[:, 1:, :]) -
                    torch.abs(Y_pred[:, :-1, :] - Y_pred[:, 1:, :]), self.alpha)
 
+        print(t1.shape)
+        print(t2.shape)
+        
         error = torch.add(t1, t2)
         loss = torch.sum(error)
         return loss 
